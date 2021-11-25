a09c562263a949abbac03df7c5a7e96e36fd8145
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2021-11-26 00:32:15 +0530

    Added comments

--- server/utils/connectDB.go
+++ server/utils/connectDB.go
@@ -16,24 +16,36 @@ func ConnectToMongoDB() {
 	//.env Variable
 	MongoURI := "mongodb+srv://kevkanae:crysis123@cluster0.etamm.mongodb.net/ecom?retryWrites=true&w=majority"
 
-	//
+	//Connect to MongoClient
 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoURI))
 	if err != nil {
+		fmt.Println("Error Connecting to MongoClient", err)
 		log.Fatal(err)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	//Timeout duration that we want to use when trying to connect
+	ctx, cancel := context.WithTimeout(context.Background(), 14*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
+		fmt.Println("Error Connecting to DB", err)
 		log.Fatal(err)
 	}
+
+	//keep the connection to the DB open until we're done
 	defer client.Disconnect(ctx)
 	defer cancel()
+
+	//Check if your're really connected
 	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
+		fmt.Println("Error Connecting to Client", err)
 		log.Fatal(err)
 	}
+
+	//return a list of DB's
 	databases, err := client.ListDatabaseNames(ctx, bson.M{})
 	if err != nil {
+		fmt.Println("Error Finding DB's", err)
 		log.Fatal(err)
 	}
 	fmt.Println(databases)
