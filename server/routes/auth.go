ca9c5ebc1b3ba2e99986ed967572df66a82e095b
Kevin D Goveas <kevdanngovead@gmail.com>
2022-01-29 14:04:00 +0530

    :bug: Add to Cart API fixed

--- server/routes/auth.go
+++ server/routes/auth.go
@@ -2,30 +2,22 @@ package routes
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kevkanae/e-com-use-kart/server/utils"
-	"net/http"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//reqToken := c.Request.Header.Get("Authorization")
-		//splitToken := strings.Split(reqToken, "Bearer")
-		//if len(splitToken) != 2 {
-		//	c.JSON(http.StatusUnauthorized, "Unauthorized")
-		//	c.Abort()
-		//	return
-		//}
-		//reqToken = strings.TrimSpace(splitToken[1])
-		//fmt.Println(reqToken)
-		reqCookie, err := c.Cookie("Eat_My_Cookie")
-		if err !=nil {
+
+		_, err := c.Cookie("Eat_My_Cookie")
+		if err != nil {
 			fmt.Println(utils.Wrap(err, "I need a Cookie ;__;"))
 			c.JSON(http.StatusUnauthorized, "Unauthorized")
 			c.Abort()
 			return
 		}
-		fmt.Println(reqCookie)
 		c.Next()
 	}
 }
