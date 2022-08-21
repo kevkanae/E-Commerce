e09d5c4f7bfb21e3ae2834f93346cd73fa364e82
Kevin Daniel Goveas <kevdanngovead@gmail.com>
2022-08-21 18:16:55 +0400

    Fixed image grids

--- src/components/Home/ImageGrid.tsx
+++ src/components/Home/ImageGrid.tsx
@@ -1,83 +1,114 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
-import BG from "../../assets/bg.jpg";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Image,
+  Text,
+} from "@chakra-ui/react";
+import { useQuery } from "urql";
+import { IGetSomeProductsQuery } from "../../interfaces/Product/IGetSomeProducts";
+import Loader from "../../layout/Loader";
+import { BsChevronCompactDown } from "react-icons/bs";
+
+const GetSomeProductsQuery = `
+  query{
+    getSomeProducts {
+      data {
+        name
+        image_url
+      } 
+      error
+      message
+    }
+  }
+`;
 
 const ImageGrid = () => {
+  const [{ data, error, fetching }, reexecuteQuery] =
+    useQuery<IGetSomeProductsQuery>({
+      query: GetSomeProductsQuery,
+    });
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
   return (
     <>
-      <Flex
-        display={{
-          base: "none",
-          md: "flex",
-        }}
-        h="100vh"
-        w="full"
-        align="center"
-        justify="center"
-        position="relative"
-      >
-        <Box h="77vh" w="48%" mr={3}>
-          <Image
-            borderRadius="0.4rem"
-            src="https://picsum.photos/777"
-            alt=""
-            h="full"
+      {data ? (
+        <>
+          <Grid
+            h="100vh"
             w="full"
-            loading="lazy"
-            objectFit="cover"
-          />
-        </Box>
-        <Flex
-          h="77vh"
-          w="30%"
-          align="center"
-          direction="column"
-          justify="space-between"
-        >
-          <Box h="37vh" w="full">
-            <Image
-              borderRadius="0.4rem"
-              src="https://picsum.photos/777"
-              alt=""
-              h="full"
+            position="relative"
+            templateColumns="repeat(3, 25vw)"
+            columnGap={7}
+            py={4}
+            px={14}
+            alignItems="center"
+            justifyContent="center"
+          >
+            {data.getSomeProducts.data.map((x, i) => (
+              <Flex
+                key={i}
+                direction="column"
+                align="center"
+                justify="center"
+                h="42vh"
+                w="25vw"
+              >
+                <Box
+                  h="84%"
+                  w="full"
+                  _hover={{
+                    cursor: "pointer",
+                    filter: "drop-shadow(0 0 0.4rem #aaa)",
+                  }}
+                >
+                  <Image
+                    src={x.image_url}
+                    fallbackSrc="https://via.placeholder.com/150"
+                    alt={x.name}
+                    borderRadius="md"
+                    objectFit="cover"
+                    h="full"
+                    w="full"
+                  />
+                </Box>
+                <Text fontWeight={600} mt={1}>
+                  {x.name}
+                </Text>
+              </Flex>
+            ))}
+            <Flex
               w="full"
-              loading="lazy"
-              objectFit="cover"
-            />
-          </Box>
-          <Box h="37vh" w="full">
-            <Image
-              borderRadius="0.4rem"
-              src="https://picsum.photos/777"
-              alt=""
-              h="full"
-              w="full"
-              loading="lazy"
-              objectFit="cover"
-            />
-          </Box>
-        </Flex>
-        <Flex
-          h="10rem"
-          w="10rem"
-          bg="teal.800"
-          borderRadius="50%"
-          position="absolute"
-          left="59%"
-          top="50%"
-          transform="translate(-50%,-50%)"
-          zIndex={777}
-          align="center"
-          justify="center"
-          _hover={{
-            cursor: "pointer",
-            filter: "drop-shadow(0 0 2em #adebebaa)",
-          }}
-        >
-          <Text fontWeight={600} color="white" letterSpacing={2}>
-            Be your Best
-          </Text>
-        </Flex>
-      </Flex>
+              h="21vh"
+              direction="column"
+              align="center"
+              justify="center"
+              position="absolute"
+              bottom="6vh"
+              left={0}
+              bg={`linear-gradient(to bottom,
+                rgba(255,255,255,0),
+                rgba(255,255,255,0.1),
+                rgba(255,255,255,0.2),
+                rgba(255,255,255,0.4),
+                rgba(255,255,255,0.6),
+                rgba(255,255,255,0.8),
+                rgba(255,255,255,1))`}
+            >
+              <Button>View More</Button>
+              <Text>
+                <BsChevronCompactDown size={28} />
+              </Text>
+            </Flex>
+          </Grid>
+        </>
+      ) : (
+        <Loader />
+      )}
     </>
   );
 };
