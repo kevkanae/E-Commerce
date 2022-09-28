b0ae88824540e338a5840aff26e8d8fd6b61c43a
Kevin Daniel Goveas <kevdanngovead@gmail.com>
2022-09-28 11:07:04 +0400

    feat: added responsiveness + added animation

--- src/components/Landing/HeroSection.tsx
+++ src/components/Landing/HeroSection.tsx
@@ -1,54 +1,122 @@
 import { useNavigate } from "react-router-dom";
 import Img from "../../assets/auth/landing.png";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
-import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  chakra,
+  Flex,
+  Image,
+  shouldForwardProp,
+  Text,
+} from "@chakra-ui/react";
+import { isValidMotionProp, motion } from "framer-motion";
 
 const HeroSection = () => {
   const navigate = useNavigate();
 
+  const ChakraBox = chakra(motion.div, {
+    shouldForwardProp: (prop) =>
+      isValidMotionProp(prop) || shouldForwardProp(prop),
+  });
+
   return (
     <>
-      <Box w="full" position="relative" bg="bg.main" h="100vh">
-        <Flex
-          h="100vh"
-          justifyContent="space-between"
-          alignItems="center"
-          gap="2rem"
+      <Flex
+        w="full"
+        h="100vh"
+        bg="bg.main"
+        position="relative"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box
+          w={{
+            sm: "100%",
+            md: "40%",
+          }}
+          color="text.main"
+          pl="10"
         >
-          <Box pb="5rem" alignSelf="flex-end" w="40%" color="text.main" pl="10">
-            <Text fontSize="2rem" fontWeight={900} color="text.light">
-              Get All Your Dressing Needs At One Stop
-            </Text>
-            <Box pt={7}>
-              <Text lineHeight="6rem" fontSize="6rem" fontWeight={900}>
-                SHOP
-              </Text>
-              <Text
-                display="inline-block"
-                lineHeight="4.6875rem"
-                bg="teal.main"
-                fontSize="6rem"
-                color="bg.secondary"
-                fontWeight="900"
-              >
-                NOW
-              </Text>
-            </Box>
-            <Button
-              rightIcon={<ArrowForwardIcon />}
-              colorScheme="teal"
-              variant="outline"
-              mt={2}
-              onClick={() => navigate("/home")}
+          <Text
+            fontSize={{
+              sm: "2rem",
+              md: "1.4rem",
+              lg: "2rem",
+            }}
+            fontWeight={900}
+            color="text.light"
+          >
+            Get All Your Dressing Needs At One Stop
+          </Text>
+          <Box
+            pt={{
+              md: 3,
+              lg: 5,
+            }}
+          >
+            <Text
+              fontSize={{
+                sm: "4rem",
+                md: "3rem",
+                lg: "6rem",
+              }}
+              fontWeight={900}
             >
-              Take Me There
-            </Button>
+              SHOP
+            </Text>
+            <Text
+              display="inline-block"
+              lineHeight={{
+                sm: "4rem",
+                md: "3rem",
+                lg: "6rem",
+              }}
+              bg="teal.main"
+              fontSize={{
+                sm: "4rem",
+                md: "3rem",
+                lg: "6rem",
+              }}
+              color="bg.secondary"
+              fontWeight="900"
+            >
+              NOW
+            </Text>
           </Box>
-          <Flex justifyContent="flex-end" alignSelf="flex-end" w="60%">
-            <Image w="90%" src={Img} />
-          </Flex>
-        </Flex>
-      </Box>
+          <Button
+            rightIcon={<ArrowForwardIcon />}
+            colorScheme="teal"
+            variant="outline"
+            mt={{
+              sm: 3,
+              md: 2,
+            }}
+            onClick={() => navigate("/home")}
+          >
+            Take Me There
+          </Button>
+        </Box>
+        <ChakraBox
+          display={{
+            sm: "none",
+            md: "flex",
+          }}
+          justifyContent="flex-end"
+          alignSelf="flex-end"
+          w="60%"
+          animate={{ y: [-7, 7] }}
+          // @ts-ignore
+          transition={{
+            duration: 3,
+            type: "ease-in-out",
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+        >
+          <Image w="90%" src={Img} />
+        </ChakraBox>
+      </Flex>
     </>
   );
 };
